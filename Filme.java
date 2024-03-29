2b7a8648a57f6977ea16c13bcb8d226541377edb
Vinicius <viniciusquintian@gmail.com>
2024-03-28 21:49:14 -0300

    main

--- Filme.java
+++ Filme.java
@@ -1,5 +1,13 @@
 public class Filme {
-  public static void main(String[] args) {
-   
+  String nome;
+  int anoDeLancamento;
+  boolean incluidoNoPlano;
+  double avaliacao;
+  int totalDeAvalicacoes;
+  int duracaoEmMinutos;
+  
+  void exibeFichaTecnica() {
+    System.out.println("nome do filme: " + nome);
+    System.out.println("Ano de lançamento: " + anoDeLancamento);
   }
 }
---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    Filme meuFilme = new Filme();
+    meuFilme.nome = "Kung fu panda 4";
+    meuFilme.anoDeLancamento = 2024;
+    meuFilme.duracaoEmMinutos = 94;
+
+    meuFilme.exibeFichaTecnica();
+  }
+}
